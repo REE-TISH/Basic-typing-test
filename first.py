c6e31c498b581ee1adb2bad6ccd99d75caf541ae
REE-TISH <reetishk798@gmail.com>
2025-04-10 00:34:40 +0530

    Update first.py

--- first.py
+++ first.py
@@ -84,25 +84,6 @@ result_label = Label(window,text="",font=("arial",14))
 
 
 window.mainloop()
-# paragraph = generate_paragraph()
-# print(paragraph)
-# paraList = paragraph.split()
-# start = time()
-# text = input("Start:").split()
-# end = time()
-# rightWords = 0
-# wrongWords = 0
-# for i in range(len(text)):
-#     if paraList[i] != text[i]:
-#         wrongWords += 1
-#     else:
-#         rightWords += 1 
-# time = end - start
-# wpm = int((rightWords/(end - start))*60)
-# if wpm == 0:
-#     print("Speed:",0,"accuracy:100%")
-# else:
-#     print("wpm:",wpm,"accuracy:",(rightWords/(rightWords + wrongWords))*100,"%")
 
 
 
